1721fe2d12ff20ea2ac67ac06e676458b5422ba3
Eric Cariat <eric_cariat@yahoo.com>
2022-11-21 23:38:50 +0100

    Add "stranger voting" title & background & css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { EthProvider } from "./contexts/EthContext";
 import Web3stuff from "./components/Web3stuff";
 import Footer from "./components/Footer";
+import Logo from "./components/Web3stuff/Logo";
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
         <div className="container">
           <Web3stuff />
           <Footer />
+          <Logo />
         </div>
       </div>
     </EthProvider>
